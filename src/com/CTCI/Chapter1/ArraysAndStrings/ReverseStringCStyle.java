d87869648f57d25425e4dfe580d2361f6d74d8d3
savanibharat <savanibharat@gmail.com>
2014-04-21 03:31:08 -0700

    reverse a string in C

---
+++ src/com/CTCI/Chapter1/ArraysAndStrings/ReverseStringCStyle.java
@@ -0,0 +1,22 @@
+package com.CTCI.Chapter1.ArraysAndStrings;
+
+public class ReverseStringCStyle {
+/*
+ * 
+ * 
+ Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+ 
+void reverse(char s[])
+{
+      int length = strlen(s) ;
+      int c, i, j;
+
+      for (i = 0, j = length - 1; i < j; i++, j--)
+     {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+      }
+}
+  */
+}
